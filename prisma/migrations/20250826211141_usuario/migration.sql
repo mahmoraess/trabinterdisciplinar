df382ce5187d99b35f796dacaa80ca89e87f98fe
mahmoraes <maria.moraes6@estudante.ifms.edu.br>
2025-08-26 17:28:13 -0400

    organizando as pastas e o prisma 26/08

---
+++ prisma/migrations/20250826211141_usuario/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "usuario" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "usuario_email_key" ON "usuario"("email");
