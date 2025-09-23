55d53aaa761cfddec7c389d3377866c49a8b83f0
mahmoraes <maria.moraes6@estudante.ifms.edu.br>
2025-09-23 17:23:16 -0400

    resolvendo o index, routes e prisma

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-//teste
+const express = require("express");
+const app = express();
+
+const usuarioRoutes = require("./routes/usuarioRoutes.js");
+app.use("/usuarios", usuarioRoutes);
+
+app.listen(8000, (err) => {
+    console.log("Aplicação rodando em localhost:8000");
+});
