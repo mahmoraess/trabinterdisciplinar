5f02f0915fd8fdece4ed56c86e2a7f8a220cac44
mahmoraes <maria.moraes6@estudante.ifms.edu.br>
2025-10-07 17:05:16 -0400

    atualizando controller

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -1,63 +1,67 @@
-const path = require("path");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-
 const { PrismaClient } = require("@prisma/client");
 const client = new PrismaClient();
 
 class UsuarioController {
-    static async cadastrar(req, res) {
-        const{ nome, email, senha} = req.body;
 
-        const salt = bcryptjs.genSaltSync(8);
-        const hashSenha = bcryptjs.hashSync(senha, salt);
+  static async cadastrar(req, res) {
+    const { nome, email, senha } = req.body;
 
-        const usuario = await client.usuario.create({
-            data: {
-                nome,
-                email,
-                senha:hashSenha,
-            },
-        })
+    const salt = bcryptjs.genSaltSync(8);
+    const hashSenha = bcryptjs.hashSync(senha, salt);
 
-        res.json({
-            usuarioId: usuario.id,
-        });
+    const usuario = await client.usuario.create({
+      data: {
+        nome,
+        email,
+        senha: hashSenha,
+      },
+    });
 
+    const token = jwt.sign({ id: usuario.id }, process.env.SENHA_SERVIDOR, {
+      expiresIn: "1h",
+    });
+
+    res.json({
+      erro: false,
+      msg: "Usuário cadastrado com sucesso!",
+      token: token,
+    });
+  }
+
+  static async login(req, res) {
+    const { email, senha } = req.body;
+
+    const usuario = await client.usuario.findUnique({
+      where: { email: email },
+    });
+
+    if (!usuario) {
+      return res.json({
+        erro: true,
+        msg: "Usuário não encontrado!",
+      });
     }
 
-    static async login(req, res) {
-        const {email, senha} = req.body;
-
-        //Verificar se usuário existe
-        const usuario = await client.usuario.findUnique({
-            where: {
-                email: email,
-            },
-        });
-        if(!usuario){
-            return res.json({
-                msg: "Usuário não encontrado!",
-            });
-        }
-
-        //Verificar se a senha está correta
-        const senhaCorreta = bcryptjs.compareSync(senha, usuario.senha);
-        if (!senhaCorreta) {
-            return res.json({
-                msg: "Senha incorreta!",
-            });
-        }
-
-        //Emitir um token 
-        const token = jwt.sign({ id: usuario.id }, process.env.SENHA_SERVIDOR, {
-            expiresIn: "1h",
-            });  
-            res.json({
-                msg: "Autenticado!",
-                token: token,
-            })
+    const senhaCorreta = bcryptjs.compareSync(senha, usuario.senha);
+    if (!senhaCorreta) {
+      return res.json({
+        erro: true,
+        msg: "Senha incorreta!",
+      });
     }
+
+    const token = jwt.sign({ id: usuario.id }, process.env.SENHA_SERVIDOR, {
+      expiresIn: "1h",
+    });
+
+    res.json({
+      erro: false,
+      msg: "Autenticado!",
+      token: token,
+    });
+  }
 }
 
 module.exports = UsuarioController;
